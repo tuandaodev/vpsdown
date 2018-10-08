0a4e835d421fc84de3d7570c02a7dfe34eb971fe
tuandaodev <tuandao.dev@gmail.com>
2018-10-09 02:17:35 +0700

    up

--- download.php
+++ download.php
@@ -1,21 +1,42 @@
-<?php 
+<?php
 
-if (ob_get_level()) ob_end_clean();
+if (ob_get_level())
+    ob_end_clean();
 
-$file_url = 'https://archive.org/download/apkmodeio/14182-MORTAL-KOMBAT-X-v1-19-0-cache-Tegra.zip';
+//$file_url = 'https://archive.org/download/apkmodeio/14182-MORTAL-KOMBAT-X-v1-19-0-cache-Tegra.zip';
+$file_url = 'https://apkmemory.com/wl/?id=v7VB7KqqWPOwCxtUApJV0V2NNHCoPWTc';
+$filename = basename($file_url);
 
-$head = array_change_key_case(get_headers($file_url, TRUE));
-$data_size = $head['content-length'];
+$response_headers = array_change_key_case(get_headers($file_url, TRUE));
+
+// Get data size
+$data_size = 0;
+if (isset($response_headers['content-length'])) {
+    $data_size = $response_headers['content-length'];
+}
+
+// Get File Name
+if (isset($response_headers["content-disposition"])) {
+    // this catches filenames between Quotes
+    if (preg_match('/.*filename=[\'\"]([^\'\"]+)/', $response_headers["content-disposition"], $matches)) {
+        $filename = $matches[1];
+    }
+    // if filename is not quoted, we take all until the next space
+    else if (preg_match("/.*filename=([^ ]+)/", $response_headers["content-disposition"], $matches)) {
+        $filename = $matches[1];
+    }
+}
 
 header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
+header("Content-Transfer-Encoding: Binary");
+header("Content-disposition: attachment; filename=\"" . $filename . "\"");
 header('Content-Transfer-Encoding: chunked'); //changed to chunked
 header('Expires: 0');
-header("Content-length: $data_size");
+if ($data_size) {
+    header("Content-length: $data_size");
+}
 header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
 header('Pragma: public');
 
 readfile($file_url); // do the double-download-dance (dirty but worky)
-
 ?>
